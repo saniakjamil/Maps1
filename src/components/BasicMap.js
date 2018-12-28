1f38a4125cba7d229d25b2ca96bb52176602fe1d
Sania K. Jamil <sania.jamil@venturedive.com>
2018-12-28 11:09:11 +0500

    Basic structure

---
+++ src/components/BasicMap.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render() {
+      return (
+        <div >
+          <h1>Sania</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Map;
+  
